5c74af68fe66d0b3bffe7af60e628c5083a5a79c
Creative280 <Alejandro.taborda280@gmail.com>
2025-07-19 23:44:59 -0500

    Agregar implementación básica de una API con FastAPI, incluyendo métodos GET, POST, PUT y DELETE. Se añade archivo de requisitos para las dependencias fastapi y uvicorn.

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, Form
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class LoginEsquema(BaseModel):
+    correo: str
+    password: str
+
+
+@app.get("/ejemplo")
+async def ejemplo():
+    return "Metodo GET"
+
+@app.get("/ejemplo/{id}")
+async def ejemplo_id(id:int):
+    return f"Metodo GET | Parametro = {type(id)} | El Valor es = {id}"
+
+@app.get("/ejemplo-query")
+async def ejemplo_query(id, slug):
+    return f"id={id} y el Slug = {slug}"
+
+@app.post("/ejemplo-form")
+async def ejmeplo_post(correo: str=Form(), password:str=Form()):
+    
+    return {"Correo": correo, "Password": password}
+
+
+@app.post("/ejemplo-request")
+async def ejmeplo_request(model:LoginEsquema):
+    return model
+
+
+@app.put("/ejemplo")
+async def ejmeplo_put():
+    return "Metodo PUT"
+
+
+
+@app.delete("/ejemplo")
+async def ejmeplo_delete():
+    return "Metodo DELETE"
